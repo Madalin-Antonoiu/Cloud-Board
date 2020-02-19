260d5b648b967b57efa89b53e9fa2cbd0ec71886
@madalin-antonoiu <antonoiumadalin@gmail.com>
2020-02-19 17:15:49 +0200

    7. Toolbar

---
+++ client/src/components/Navbar.vue
@@ -0,0 +1,21 @@
+<template>
+	<nav>
+		<v-toolbar flat app>
+			<v-toolbar-title class="text-uppercase grey--text">
+				<span class="font-weight-light">Cloud</span>
+				<span>.admin</span>
+			</v-toolbar-title>
+			<v-spacer></v-spacer>
+			<v-btn text color="grey" small >
+				<span class="grey--text">Sign Out</span>
+				<v-icon right color="grey">mdi-exit-to-app</v-icon>
+			</v-btn>
+		</v-toolbar>
+	</nav>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
