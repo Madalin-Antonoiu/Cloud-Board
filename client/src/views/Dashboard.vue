45be2768eba589aaa3a10f87aebe5377b023811a
@madalin-antonoiu <antonoiumadalin@gmail.com>
2020-02-20 09:03:30 +0200

    Default Padding & Margin App

--- client/src/views/Dashboard.vue
+++ client/src/views/Dashboard.vue
@@ -1,15 +1,23 @@
 <template>
   <div class="dashboard">
-    <!-- <Navbar /> -->
-    <p>This is the default dashboard page</p>
+   
+    <v-subheader class="px-0 grey--text">
+      Dashboard
+    </v-subheader>
+
+    <v-container class="my-5">
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque eaque quos doloremque, sit perferendis quidem vel ea? Quae voluptates, laborum atque quaerat suscipit error et facere, quidem velit praesentium eos.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur ipsum doloribus rem officia eius. Quos dolorem quod ut magnam sit nisi delectus quis asperiores? Est pariatur harum fugit quasi provident?</p>
+    </v-container>
+
   </div>
 </template>
 
 <script>
-// import Navbar from '@/components/Navbar'
+
 
 export default {
   name: 'Dashboard',
-  // components: { Navbar}
+
 }
 </script>
--- client/src/views/Notes.vue
+++ client/src/views/Notes.vue
@@ -1,15 +1,22 @@
 <template>
   <div class="notes">
-    <!-- <Navbar /> -->
-    <p>This is a notes about page</p>
+ 
+    <v-subheader class="px-0 grey--text">
+      Notes
+    </v-subheader>
+      
+    <v-container>
+      Notes content
+    </v-container>
+
   </div>
 </template>
 
 <script>
-// import Navbar from '@/components/Navbar'
+
 
 export default {
   name: 'About',
-  //  components: { Navbar}
+
 }
 </script>
