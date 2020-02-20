c5632494c6f1467abd2c5cf7c7b659c09d548bb6
@madalin-antonoiu <antonoiumadalin@gmail.com>
2020-02-20 09:35:36 +0200

    Grid System 2 : Notes

--- client/src/views/Notes.vue
+++ client/src/views/Notes.vue
@@ -5,8 +5,32 @@
       Notes
     </v-subheader>
       
-    <v-container>
-      Notes content
+    <v-container class="my-5">
+      <v-card flat class="pa-3">
+        <v-layout row class="mx-0">
+
+          <v-flex xs12 md6>
+            <div class="caption grey--text">Title</div>
+            <div>Vuetify Course 1</div>
+          </v-flex>
+
+          <v-flex xs6 sm4 md2>
+            <div class="caption grey--text">Owner</div>
+            <div>Madalin</div>
+          </v-flex>
+          
+          <v-flex xs6 sm4 md2>
+            <div class="caption grey--text">Viewed</div>
+            <div>5 minutes ago</div>
+          </v-flex>
+
+          <v-flex xs6 sm4 md2>
+            <div class="caption grey--text">Group</div>
+            <div>Vuetify</div>
+          </v-flex>
+
+        </v-layout>
+      </v-card>
     </v-container>
 
   </div>
