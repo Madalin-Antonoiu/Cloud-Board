8d40326b68704609e748071f505621a884081152
@madalin-antonoiu <antonoiumadalin@gmail.com>
2020-02-20 11:42:20 +0200

    Added secondary nested list

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -46,6 +46,34 @@
 			
 			</v-list>
 
+			<v-list > <!-- color="white" -->
+
+      <v-list-group
+        v-for="item in items"
+        :key="item.title"
+        v-model="item.active"
+        :prepend-icon="item.action"
+        no-action
+				color="white"
+      >
+        <template v-slot:activator>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title"></v-list-item-title>
+          </v-list-item-content>
+        </template>
+
+        <v-list-item
+          v-for="subItem in item.items"
+          :key="subItem.title"
+          @click="`/`"
+        >
+          <v-list-item-content>
+            <v-list-item-title v-text="subItem.title"></v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list-group>
+    </v-list>
+
 		</v-navigation-drawer>
 
 	</nav>
@@ -59,10 +87,69 @@ export default {
 				// watcher: false,
 				links: [
 					{icon: 'dashboard', text: 'Dashboard', route: '/mantonoiu'}, //hardcoded username
-					{icon: 'folder', text: 'Notes', route: '/mantonoiu/notes/'},
+					{icon: 'folder', text: 'Notes', route: '/mantonoiu/notes/',
+						nested1:['VueJs', 'VavaScript'] 
+					},
 					{icon: 'chat', text: 'Chat', route: '/mantonoiu/chat'},
 					{icon: 'fab fa-youtube', text: 'Youtube', route: '/mantonoiu/youtube'},
-				]
-  }),
-}
+				],
+				items: [
+				{
+				action: 'local_activity',
+				title: 'Attractions',
+				items: [
+				{ title: 'List Item' },
+				],
+				},
+				{
+				action: 'restaurant',
+				title: 'Dining',
+				active: true,
+				items: [
+				{ title: 'Breakfast & brunch' },
+				{ title: 'New American' },
+				{ title: 'Sushi' },
+				],
+				},
+				{
+				action: 'school',
+				title: 'Education',
+				items: [
+				{ title: 'List Item' },
+				],
+				},
+				{
+				action: 'directions_run',
+				title: 'Family',
+				items: [
+				{ title: 'List Item' },
+				],
+				},
+				{
+				action: 'healing',
+				title: 'Health',
+				items: [
+				{ title: 'List Item' },
+				],
+				},
+				{
+				action: 'content_cut',
+				title: 'Office',
+				items: [
+				{ title: 'List Item' },
+				],
+				},
+				{
+				action: 'local_offer',
+				title: 'Promotions',
+				items: [
+				{ title: 'List Item' },
+				],
+				},
+				],
+
+
+
+				}
+				)}
 </script>
