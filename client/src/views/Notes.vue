dce93414db86472608c5452be97554c9f1a65c6d
@madalin-antonoiu <antonoiumadalin@gmail.com>
2020-02-20 17:18:12 +0200

    Added v-table

--- client/src/views/Notes.vue
+++ client/src/views/Notes.vue
@@ -20,39 +20,65 @@
           </v-tab>
 
           <!-- Same for its content, JSON based -->
-      
-          <v-tab-item  v-for="group in groups" :key="group.id">
-
-            <v-card flat class="pa-3" v-for="note in group.notes" :key="note.title" >
-              <v-layout row class="mx-0">
-
-                <v-flex xs12 md6>
-                  <div class="caption grey--text">Title</div>
-                  <div>{{note.title}}</div>
-                </v-flex>
-
-                <v-flex xs6 sm4 md2>
-                  <div class="caption grey--text">Owner</div>
-                  <div>{{note.owner}}</div>
-                </v-flex>
-                
-                <v-flex xs6 sm4 md2>
-                  <div class="caption grey--text">Seen</div>
-                  <div>{{note.seen}}</div>
-                </v-flex>
-
-                <v-flex xs6 sm4 md2>
-                  <div class="caption grey--text">Tag</div>
-                  <div>{{note.tag}}</div>
-                </v-flex>
-              </v-layout>
-            </v-card>   
-          </v-tab-item>
      
+          <v-tab-item  v-for="group in groups" :key="group.id">
+           
+            <v-card flat> <!-- The card component that holds next card, so i only create one table-->
+
+                <v-card flat class="pa-3" v-for="note in group.notes" :key="note.title" >
+                  <v-layout row class="mx-0">
+
+                    <v-flex xs12 md6>
+                      <div class="caption grey--text">Title</div>
+                      <div>{{note.title}}</div>
+                    </v-flex>
+
+                    <v-flex xs6 sm4 md2>
+                      <div class="caption grey--text">Owner</div>
+                      <div>{{note.owner}}</div>
+                    </v-flex>
+                    
+                    <v-flex xs6 sm4 md2>
+                      <div class="caption grey--text">Seen</div>
+                      <div>{{note.seen}}</div>
+                    </v-flex>
+
+                    <v-flex xs6 sm4 md2>
+                      <div class="caption grey--text">Tag</div>
+                      <div>{{note.tag}}</div>
+                    </v-flex>
+
+                  </v-layout>
+                </v-card>  
+
+                <hr>
+
+                <!-- The table (delete hr)-->
+                <v-card-title>
+                  Nutrition
+                  <v-spacer></v-spacer>
+                  <v-text-field
+                    v-model="search"
+                    append-icon="search"
+                    label="Search"
+                    single-line
+                    hide-details
+                  ></v-text-field>
+                </v-card-title>
+
+                <v-data-table
+                    :headers="headers"
+                    :items="desserts"
+                    :search="search"
+                ></v-data-table>
+
+            </v-card>
+
+          </v-tab-item>
 
         </v-tabs>
-
-      </v-card> 
+      </v-card>
+     
 
     </v-container>
   </div>
@@ -127,6 +153,104 @@ export default {
         //     ],
       // }, 
     },
+    search: '',
+    headers: [
+      {
+        text: 'Dessert (100g serving)',
+        align: 'left',
+        sortable: false,
+        value: 'name',
+      },
+      { text: 'Calories', value: 'calories' },
+      { text: 'Fat (g)', value: 'fat' },
+      { text: 'Carbs (g)', value: 'carbs' },
+      { text: 'Protein (g)', value: 'protein' },
+      { text: 'Iron (%)', value: 'iron' },
+    ],
+    desserts: [
+      {
+        name: 'Frozen Yogurt',
+        calories: 159,
+        fat: 6.0,
+        carbs: 24,
+        protein: 4.0,
+        iron: '1%',
+      },
+      {
+        name: 'Ice cream sandwich',
+        calories: 237,
+        fat: 9.0,
+        carbs: 37,
+        protein: 4.3,
+        iron: '1%',
+      },
+      {
+        name: 'Eclair',
+        calories: 262,
+        fat: 16.0,
+        carbs: 23,
+        protein: 6.0,
+        iron: '7%',
+      },
+      {
+        name: 'Cupcake',
+        calories: 305,
+        fat: 3.7,
+        carbs: 67,
+        protein: 4.3,
+        iron: '8%',
+      },
+      {
+        name: 'Gingerbread',
+        calories: 356,
+        fat: 16.0,
+        carbs: 49,
+        protein: 3.9,
+        iron: '16%',
+      },
+      {
+        name: 'Jelly bean',
+        calories: 375,
+        fat: 0.0,
+        carbs: 94,
+        protein: 0.0,
+        iron: '0%',
+      },
+      {
+        name: 'Lollipop',
+        calories: 392,
+        fat: 0.2,
+        carbs: 98,
+        protein: 0,
+        iron: '2%',
+      },
+      {
+        name: 'Honeycomb',
+        calories: 408,
+        fat: 3.2,
+        carbs: 87,
+        protein: 6.5,
+        iron: '45%',
+      },
+      {
+        name: 'Donut',
+        calories: 452,
+        fat: 25.0,
+        carbs: 51,
+        protein: 4.9,
+        iron: '22%',
+      },
+      {
+        name: 'KitKat',
+        calories: 518,
+        fat: 26.0,
+        carbs: 65,
+        protein: 7,
+        iron: '6%',
+      },
+    ],
+     
+
   }),
 
 }
