0f80112c512f0f47718893f97701767fd5f42638
@madalin-antonoiu <antonoiumadalin@gmail.com>
2020-02-21 09:25:16 +0200

    Table Mobile Breakpoint = NaN + Badges change dinamically

--- client/src/views/Notes.vue
+++ client/src/views/Notes.vue
@@ -28,7 +28,7 @@
                 <!-- Toolbar above Table-->
                 <v-toolbar dense flat class="mt-1"> <!--style=" opacity: 0.6 " -->
 
-                  <v-btn class="ma-2" outlined small fab color="primary">
+                  <v-btn class="ma-2" outlined small fab color="primary" @click="doTHis">
                     <v-icon >post_add</v-icon>
                   </v-btn>
 
@@ -84,22 +84,26 @@
 
                   </v-layout> -->
                    <!-- :hide-default-header="$vuetify.breakpoint.mdAndDown" -->
-                 <!--:dense="$vuetify.breakpoint.mdAndDown" -->
-                    <v-data-table ref="tableC"
-                      
-                      dense
+                 <!--:dense="$vuetify.breakpoint.mdAndDown" 
+                  && dog == 'heightAuto'
+                 -->
+                    <v-data-table id="table" ref="tableC"
+                      :dense="$vuetify.breakpoint.mdAndDown"
                       :headers="headers"
                       :items="group.notes"
                       :search="search"
+                      :mobile-breakpoint='NaN'
                     >
 
                     <template v-slot:item.completed="{ item }">
                       <v-simple-checkbox v-model="item.completed" disabled></v-simple-checkbox>
                     </template>
                       <!--this.$refs.table.hasAttribute('dense') -->
+                      <!-- style="height: auto"  only when v-data-table has dense-->
                       <template v-slot:item.tag="{ item }">
                         <v-chip :color="getColor(item.tag)" dark 
-                        :class="{'heightAuto': editMeHereYouWereLast }" 
+                        :class="{'heightAuto' : $vuetify.breakpoint.mdAndDown}"
+                        
                         >{{ item.tag }}</v-chip>
                       </template>
 
@@ -132,109 +136,118 @@
 export default {
   name: 'About',
   // Written like this, data doesn't need return {}
-  data: () => ({
-    yes: true,
-    groups:{
-      Vuetify :{
-          name: 'Vuetify', 
-          icon: 'logos:vuetifyjs',
-          notes: [
-            { 
-              title: 'Introduction to Vuetify',
-              owner: 'Madochan', 
-              seen: "1 week ago", 
-              tag: 'Course1', 
-              content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Id amet itaque minima quae. Sapiente ipsam ab eligendi fugiat debitis nesciunt corrupti repellat asperiores vero qui doloremque maiores cum, consequatur ea?' ,
-              completed: true,
-            },
-            { //4
-              title: 'Testing Vuetify - simple job!', 
-              owner: 'Madut', 
-              seen: "3 min ago", 
-              tag: 'Precious', 
-              content:'  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores optio corrupti, ipsa nisi voluptatum rem beatae eaque est consequatur labore, ipsam iure at accusamus quisquam incidunt ipsum! Deserunt, ullam? Sed.' 
-            },
-          ],
-      },      
-      JavaScript :{
-          name: 'JavaScript', 
-          icon: 'logos:javascript',
-          notes: [
-            { 
-              title: 'Classes and colors in JS', 
-              owner: 'Ana', 
-              seen: "4 days ago", 
-              tag: 'ToLearn', 
-              content:'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Commodi quis accusantium sit numquam nobis quasi incidunt aut dolores, similique obcaecati id animi cupiditate deleniti voluptas nisi ipsam minima qui assumenda.' 
-            },
-          ],
-      }, 
-      VueJs :{
-          name: 'VueJs', 
-          icon: 'logos:vue',
-          notes: [
-            { //3
-              title: 'All about Directives in Vue', 
-              owner: 'Christian', 
-              seen: "1 month ago", 
-              tag: 'MustDo', 
-              content:'  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores optio corrupti, ipsa nisi voluptatum rem beatae eaque est consequatur labore, ipsam iure at accusamus quisquam incidunt ipsum! Deserunt, ullam? Sed.' 
-            },
-          ],
-      }, 
-      // Sass :{
-        //     name: 'Sass', 
-        //     icon: 'logos:sass',
-        //     notes: [
-        //       { //3
-        //         title: 'Sass-licious!', 
-        //         owner: 'Cory', 
-        //         seen: "1 month ago", 
-        //         tag: 'MustDo', 
-        //         content:'  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores optio corrupti, ipsa nisi voluptatum rem beatae eaque est consequatur labore, ipsam iure at accusamus quisquam incidunt ipsum! Deserunt, ullam? Sed.' 
-        //       },
-        //     ],
-      // }, 
-    },
-    search: '',
-    headers: [
-      { text: 'Title', align: 'left', value: 'title'}, // sortable: false, , 
-      { text: 'Owner', value: 'owner', align: ' d-none' },
-      { text: 'Seen', value: 'seen' },
-      { text: 'Tag', value: 'tag' },
-      { text: 'Completed', value: 'completed' },
-    ],
-    rules: [v => v.length <= 14 || 'Max 15 characters'],
-  
-  }),
-    methods: {
-      getColor () {
-        //Beware, this generates random color always
-        let color = this.randomColor();
-        return color
-        // if (tag == 'Course1') return 'blue'
-        // else if (tag == "ToLearn") return 'yellow'
-        // else return color
+  data() { 
+    return { 
+      // dog: 'heightAuto',
+      yes: true,
+      obj: '',
+      groups:{
+        Vuetify :{
+            name: 'Vuetify', 
+            icon: 'logos:vuetifyjs',
+            notes: [
+              { 
+                title: 'Introduction to Vuetify',
+                owner: 'Madochan', 
+                seen: "1 week ago", 
+                tag: 'Course1', 
+                content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Id amet itaque minima quae. Sapiente ipsam ab eligendi fugiat debitis nesciunt corrupti repellat asperiores vero qui doloremque maiores cum, consequatur ea?' ,
+                completed: true,
+              },
+              { //4
+                title: 'Testing Vuetify - simple job!', 
+                owner: 'Madut', 
+                seen: "3 min ago", 
+                tag: 'Precious', 
+                content:'  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores optio corrupti, ipsa nisi voluptatum rem beatae eaque est consequatur labore, ipsam iure at accusamus quisquam incidunt ipsum! Deserunt, ullam? Sed.' 
+              },
+            ],
+        },      
+        JavaScript :{
+            name: 'JavaScript', 
+            icon: 'logos:javascript',
+            notes: [
+              { 
+                title: 'Classes and colors in JS', 
+                owner: 'Ana', 
+                seen: "4 days ago", 
+                tag: 'ToLearn', 
+                content:'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Commodi quis accusantium sit numquam nobis quasi incidunt aut dolores, similique obcaecati id animi cupiditate deleniti voluptas nisi ipsam minima qui assumenda.' 
+              },
+            ],
+        }, 
+        VueJs :{
+            name: 'VueJs', 
+            icon: 'logos:vue',
+            notes: [
+              { //3
+                title: 'All about Directives in Vue', 
+                owner: 'Christian', 
+                seen: "1 month ago", 
+                tag: 'MustDo', 
+                content:'  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores optio corrupti, ipsa nisi voluptatum rem beatae eaque est consequatur labore, ipsam iure at accusamus quisquam incidunt ipsum! Deserunt, ullam? Sed.' 
+              },
+            ],
+        }, 
+        // Sass :{
+          //     name: 'Sass', 
+          //     icon: 'logos:sass',
+          //     notes: [
+          //       { //3
+          //         title: 'Sass-licious!', 
+          //         owner: 'Cory', 
+          //         seen: "1 month ago", 
+          //         tag: 'MustDo', 
+          //         content:'  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores optio corrupti, ipsa nisi voluptatum rem beatae eaque est consequatur labore, ipsam iure at accusamus quisquam incidunt ipsum! Deserunt, ullam? Sed.' 
+          //       },
+          //     ],
+        // }, 
       },
+      search: '',
+      headers: [
+        { text: 'Title', align: 'left', value: 'title'}, // sortable: false, , 
+        { text: 'Owner', value: 'owner', align: ' d-none' },
+        { text: 'Seen', value: 'seen' },
+        { text: 'Tag', value: 'tag' },
+        { text: 'Completed', value: 'completed' },
+      ],
+      rules: [v => v.length <= 14 || 'Max 15 characters'],
+  }},
+  methods: {
+ 
+    doTHis(){
+      // let myObj = this.$refs.tableC[0]
 
-      randomColor(){
-          var num = Math.round(0xffffff * Math.random());
-          var r = num >> 16;
-          var g = num >> 8 & 255;
-          var b = num & 255;
-          return 'rgb(' + r + ', ' + g + ', ' + b + ')';
+      if(this.$refs.tableC[0].dense) {
+          this.tableIsDense="true"
       }
+      else {
+          this.tableIsDense="false"
+      }
+      
     },
+    getColor (tag) {
+      //Beware, this generates random color always
+      // let color = this.randomColor();
+      // return color
+      if (tag == 'Course1') return 'blue'
+      else if (tag == "ToLearn") return 'yellow'
+      else return 'red'
+    },
+
+    randomColor(){
+        var num = Math.round(0xffffff * Math.random());
+        var r = num >> 16;
+        var g = num >> 8 & 255;
+        var b = num & 255;
+        return 'rgb(' + r + ', ' + g + ', ' + b + ')';
+    }
+  },
 }
 </script>
 
 <style lang="css" >
-/* .iconify { color: #7BC6FF !important} */
-/* .v-text-field .v-input__control .v-input__slot {
-    height: 10px !important;
-
-  } */
-  .heightAuto{
-    height: auto;
+  .heightAuto {
+    height: auto !important;
   }
 </style>
